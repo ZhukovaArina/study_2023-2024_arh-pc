5efae9667340ad87057dad48d341c58803a4743f
Arina Zhukova <arichka2005@yandex.ru>
2023-12-08 12:15:58 +0300

    feat(main): add files lab-9

---
+++ labs/lab09/report/файлы/lab09-1.asm
@@ -0,0 +1,48 @@
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Введите x: ',0
+result: DB 'f(x)=2x+7, g(x)=3x-1, f(g(x))=',0
+
+SECTION .bss
+x: RESB 80
+res: RESB 80
+
+SECTION .text
+GLOBAL _start
+       _start:
+; Основная программа
+mov eax, msg
+call sprint
+
+mov ecx, x
+mov edx, 80
+call sread
+
+mov eax,x
+call atoi
+
+call _calcul ; Вызов подпрограммы _calcul
+
+mov eax,result
+call sprint
+mov eax,[res]
+call iprintLF
+
+call quit
+; Подпрограмма вычисления выражения "2x+7"
+_calcul:
+call _subcalcul
+mov ebx,2
+mul ebx
+add eax,7
+mov [res],eax
+
+ret ; Выход из попрограммы
+;Подпрограмма для вычисления выражения "3x-1"
+_subcalcul:
+mov ebx,3
+mul ebx
+sub eax,1
+mov [res],eax
+
+ret ; выход из подпрограммы
